2917b8a527f2a81fc21f14e5b3ec448a7812b855
diegoh27 <diegolancer27@gmail.com>
2024-01-10 02:35:11 -0400

    othersChanges

--- api/config.js
+++ api/config.js
@@ -4,7 +4,7 @@ const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_USER = process.env.DB_USER || "postgres";
 const DB_PASSWORD = process.env.DB_PASSWORD || 1234;
 const DB_NAME = process.env.DB_NAME || "food";
-const DB_PORT = process.env.DB_PORT || 3001;
+const DB_PORT = process.env.DB_PORT || 5432;
 
 module.exports = {
   PORT,
